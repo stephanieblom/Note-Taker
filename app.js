72d187c66d301cf1242b45394da94247bb55a79b
Stephanie Blom <stephblom@hotmail.com>
2020-03-04 15:26:25 -0500

    Push and delete notes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require( "express" );
+const express = require("express");
 const bodyParser = require('body-parser')
 const fs = require("fs");
 
@@ -10,67 +10,73 @@ app.use(express.static('public'));
 // needed for POST FORM decoding
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT, function() {
-    console.log(`App running on: http://localhost:${PORT}`);
-  });
+app.listen(PORT, function () {
+  console.log(`App running on: http://localhost:${PORT}`);
+});
 
 // initial sample data
-let myNotes = []; 
+let myNotes = [];
 
 // our routes
-app.get( '/notes', function( req, res ){
-  fs.readFile("db.json", "utf8", function(error, data) {
+app.get('/notes', function (req, res) {
+  fs.readFile("db.json", "utf8", function (error, data) {
 
     if (error) {
       return console.log(`error ${error}`);
     }
-  
-    if (data == undefined){
+
+    if (data == undefined || data == "") {
       console.log(`no notes to display from db`);
-    }else{
+    } else {
       myNotes = data
     }
     console.log(`My Notes: ${myNotes}`)
-  
+
   });
-    res.send( myNotes );
+  res.send(myNotes);
 });
 
-app.post( '/api/notes', function( req, res ){
-  
-    console.log( `<< inserting note: `, req.body.toString() );
-    // save the data to our table
-    myNotes.push( req.body );
-    myNotes = JSON.stringify( myNotes )
-    
-    fs.writeFile("db.json", myNotes, function(error)  {
+app.post('/api/notes', function (req, res) {
 
-      if (error) {
-        return console.log(error);
-      }
-    
-      console.log(myNotes);
-    
-    });
-    res.send( { message: `saved: ${req.body.name}` } );
+  console.log(`<< inserting note: `, req.body);
+  console.log(typeof(myNotes));
+  // save the data to our table
+  let pushNotes = JSON.parse(myNotes);
+  pushNotes.push(req.body);
+  pushNotes = JSON.stringify(pushNotes)
+
+  fs.writeFile("db.json", pushNotes, function (error) {
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log(pushNotes);
+
+  });
+  res.send({ message: `saved: ${req.body.name}` });
 });
 
-app.post( '/api/notes/:id', async function( req, res ){
+// app.post('/api/notes/:id', async function (req, res) {
 
-    const id = req.params.id;
+//   const id = req.params.id;
+//   console.log(`ID: ${id}`)
+//   console.log(`Notes: ${myNotes}`)
+//   console.log(typeof (myNotes))
 
-    myNotes.splice(id, 1)
-    console.log(`Notes: ${myNotes}`)
+//   myNotes = JSON.parse(myNotes)
+//   splicedNotes = myNotes.splice(id, 1)
+//   console.log(`Notes: ${splicedNotes}`)
 
-    fs.writeFile("db.json", myNotes, function(error)  {
+//   fs.writeFile("db.json", JSON.stringify(splicedNotes), function (error) {
 
-      if (error) {
-         console.log(error);
-      }
-    
-      console.log(myNotes);
-    
-    });
-    res.send( { message: `Finished deleting id ${id}` } );
-}) // app.delete()
+//     if (error) {
+//       console.log(error);
+//     }
+
+//     console.log(splicedNotes);
+
+//   });
+//   res.send({ message: `Finished deleting id ${id}` });
+// }) // app.delete()
 
